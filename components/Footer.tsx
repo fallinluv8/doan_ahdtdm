7a97d376452cdea522fd3efa5180df2b29066181
Eight <tung081204@gmail.com>
2024-12-05 20:46:15 +0700

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
       <div className='border-t-2 border-sky-200 mx-auto my-10'></div>
       <div className='flex flex-row mx-10 justify-around items-center'>
         <div>
-          <p>Nhosm 3TV</p>
+          <p>Group 3TV</p>
           <p>Nguyen Nhu Quang Tung</p>
           <p>Dang Quoc Thai</p>
           <p>Pham Hoang Phuc</p>
